c6251fb6ac3e68f4000af07874d6baf5955cb388
Chip Cullen <chip@chipcullen.com>
2022-04-01 10:56:47 -0400

    starting direct translation commands

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,7 @@ import { colorTypes } from './utils/colorTypes';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let translateColor = vscode.commands.registerCommand('colorosetta-extension.translateColor', () => {
-
+	const getValidInput = () => {
 		const editor = vscode.window.activeTextEditor;
 
 		if (editor) {
@@ -18,35 +17,72 @@ export function activate(context: vscode.ExtensionContext) {
 			const text = document.getText(selection);
 
 			if (!text) {
-				vscode.window.showInformationMessage('Make a selection first');
-				return;
+				vscode.window.showErrorMessage('Make a selection first');
+				return null;
 			}
 
 			const colorType = typeOfColor(text);
 
 			if (!colorType || !isValidColor(text, colorType)) {
-				vscode.window.showInformationMessage('Please input a valid color');
-				return;
+				vscode.window.showErrorMessage('Please input a valid color');
+				return null;
 			}
 
+			return {
+				text: text,
+				colorType: colorType,
+			};
+		}
+	};
+
+	const replaceEditorText = (input: string) => {
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const selection = editor.selection;
+
+			editor.edit(editBuilder => {
+				editBuilder.replace(selection, input);
+			});
+		}
+	};
+
+	let translateColor = vscode.commands.registerCommand('colorosetta-extension.translateColor', () => {
+		const input = getValidInput();
+
+		if (input) {
 			vscode.window.showQuickPick(
 				[
-					translatedColor(text, colorType, colorTypes.hex6),
-					translatedColor(text, colorType, colorTypes.hex8),
-					translatedColor(text, colorType, colorTypes.rgb),
-					translatedColor(text, colorType, colorTypes.rgba),
-					translatedColor(text, colorType, colorTypes.hsl),
-					translatedColor(text, colorType, colorTypes.hsla),
+					translatedColor(input.text, input.colorType, colorTypes.hex6),
+					translatedColor(input.text, input.colorType, colorTypes.hex8),
+					translatedColor(input.text, input.colorType, colorTypes.rgb),
+					translatedColor(input.text, input.colorType, colorTypes.rgba),
+					translatedColor(input.text, input.colorType, colorTypes.hsl),
+					translatedColor(input.text, input.colorType, colorTypes.hsla),
 		  	]
 			).then(qpSelection => {
-				editor.edit(editBuilder => {
-					editBuilder.replace(selection, qpSelection as string);
-				});
+				replaceEditorText(qpSelection as string);
 			});
 		}
 	});
 
-	context.subscriptions.push(translateColor);
+	let toRgb = vscode.commands.registerCommand('colorosetta-extension.toRgb', () => {
+		const input = getValidInput();
+
+		if (input) {
+			replaceEditorText(translatedColor(input.text, input.colorType, colorTypes.rgb));
+		}
+	});
+
+	let toRgba = vscode.commands.registerCommand('colorosetta-extension.toRgba', () => {
+		const input = getValidInput();
+
+		if (input) {
+			replaceEditorText(translatedColor(input.text, input.colorType, colorTypes.rgba));
+		}
+	});
+
+	context.subscriptions.push(translateColor, toRgb, toRgba);
 }
 
 // this method is called when your extension is deactivated
